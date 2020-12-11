2a01e058341242a0cacee6617d54f887c3b183e5
Richard Pryce <rich_pryce@yahoo.co.uk>
2020-12-11 10:02:29 +0000

    email field normalized in user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -15,3 +15,10 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'blob@BLOBBY.COM'
+        user = get_user_model().objects.create_user(email, 'password')
+
+        self.assertEqual(user.email, email.lower())
