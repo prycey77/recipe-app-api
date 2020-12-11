9640e9f797cf7303b99d930d8085693a91f3a962
Richard Pryce <rich_pryce@yahoo.co.uk>
2020-12-11 14:33:06 +0000

    Updated admin to support custom user model

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,42 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@blah.com',
+            password='password'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@blah.com',
+            password='password',
+            name='Full Name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        #Creates a url like: /admin/core/user/1 using args for id
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200) 
+
+    def test_create_user_page(self):
+        """Test that the create page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
